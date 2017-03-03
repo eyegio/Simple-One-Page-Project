f5a4643257d47900d26188f3dbbcad1b730b02bb
eyegio <g.segagni@gmail.com>
2017-03-03 11:28:32 -0500

    added video and cards

--- src/app/components/second-section/second-section.component.ts
+++ src/app/components/second-section/second-section.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-second-section',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-section.component.sass']
 })
 export class SecondSectionComponent implements OnInit {
-
-  constructor() { }
+  url: any;
+  name:string;
+  video: any = {id: '43QTjFCPLtI'};
+  baseUrl:string = 'https://www.youtube.com/embed/';
+  constructor(private sanitizer: DomSanitizer) {
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.video.id);
+  }
 
   ngOnInit() {
   }
 
 }
+
+
